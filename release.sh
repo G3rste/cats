4a1d9d2c0037f4b5231120c7d387608a622b69e5
G3rste <philippgerstenberg@web.de>
2022-05-28 22:13:24 +0200

    Add primitive release script

---
+++ release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+version=$(grep -o '"version":\s*"\([0-9]\.*\)*"' resources/modinfo.json | grep -o '\([0-9]\.*\)*')
+petai=$(grep -o '"petai":\s*"\([0-9]\.*\)*"' resources/modinfo.json | grep -o '\([0-9]\.*\)*' | sed -r 's/[0-9]*$/+/')
+releasefile='bin/cats_v'$version'_petai_v'$petai'.zip'
+dotnet build -c release
+mv bin/cats.zip $releasefile
+gh release create --generate-notes 'v'$version $releasefile 
